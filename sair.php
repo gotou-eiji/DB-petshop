e3f2d177384d8735cb7f0a26cd2a6c0c2e5c9ee1
Eiji <99904315+gotou-eiji@users.noreply.github.com>
2022-08-01 17:07:42 -0300

    Add files via upload

---
+++ sair.php
@@ -0,0 +1,8 @@
+<?php
+    session_start();
+    $user = $_SESSION["user"];
+    $_SESSION = array();
+    if(isset($_COOKIE[session_name])){
+        setcookie(session_name(),'',time()-1000,'/');
+    }
+?>
